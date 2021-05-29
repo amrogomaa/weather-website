ba02e25c30f108a66006ff72e49557f2e77a7061
amrgomaa <amroabdulbassetgomaa@gmail.com>
2021-05-29 03:18:01 +0200

    Setup app for heroku

--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ const express = require('express')
 const hbs=require('hbs')
 
 const app = express()
+const port=process.env.PORT || 3000
 
 // Define paths for Express config
 const publicDirectoryPath=path.join(__dirname,'../public')
@@ -16,7 +17,6 @@ const partialsPath=path.join(__dirname, '../tempelates/partials')
 app.set('view engine', 'hbs')
 app.set('views', viewPath)
 hbs.registerPartials(partialsPath)
-
 
 
 app.use(express.static(publicDirectoryPath));
@@ -84,6 +84,6 @@ app.get('*',(req ,res)=>{
     })
 })
 
-app.listen(3000, () => {
-    console.log('Server is up on port 3000.')
+app.listen(port, () => {
+    console.log('Server is up on port '+port)
 })
